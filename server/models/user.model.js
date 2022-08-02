45d6b996b34a0d46aff80939929af40d6ef2c17b
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-08-02 18:58:13 +0530

    finished

---
+++ server/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose  = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name: {type: String, required:true},
+    email: {type: String, required:true,unique: true},
+    password: {type: String, required:true},
+    quote: {type: String}
+},
+{
+    collection : 'user-data'
+}
+);
+const User = mongoose.model('user', userSchema);
+module.exports = User
---
+++ server/server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+
+const mongoose = require('mongoose');
+const User = require('./models/user.model');
+
+app.use(cors())
+
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://epj:epj_2708@cluster0.uklfn.mongodb.net/userdata?retryWrites=true&w=majority')
+
+app.post("/register", async (req,res) => {
+    try {
+        console.log(req.body )
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({status: 'ok'})
+    } catch (error) {
+        res.json({status: "error", error: "duplicate email"})
+    }
+})
+
+app.post("/login", async (req,res) => {
+        const user = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        })
+        if(user){
+            const token = jwt.sign({
+                name: user.name,
+                email: user.email,
+
+            }, 'secret278')
+            return res.json({status: 'ok', user: token});
+        }else{
+            return res.json({status: "error", error: false})
+        }
+})
+
+app.get("/product", async (req,res) => {
+    const token = req.headers['x-access-token'];
+    try {
+        const decode = jwt.verify(token, 'secret278')
+        const email = decode.email;
+        const user  = await User.findOne({email: email});
+        return res.json({status: 'ok', quote: user.quote})
+    } catch (error) {
+        console.log(error);
+        res.json({status: 'error', error: 'invalid token'})
+    }
+    
+})
+
+app.post("/product", async (req,res) => {
+    const token = req.headers['x-access-token'];
+    try {
+        const decode = jwt.verify(token, 'secret278')
+        const email = decode.email;
+        const user  = await User.findOne({email: email},{$set : {quote: req.body.quote}});
+        return {status: 'ok'}
+    } catch (error) {
+        console.log(error);
+        res.json({status: 'error', error: 'invalid token'})
+    }
+    
+})
+
+app.listen(8080, () => {
+    console.log('server started')
+})
